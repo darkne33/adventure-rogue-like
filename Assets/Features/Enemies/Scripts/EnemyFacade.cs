0fdd9b98d1a3ef3da61014cd317fcd7ee1269669
darkne33 <yuravislous@gmail.com>
2025-12-29 21:17:48 +0300

    base logic enemy

---
+++ Assets/Features/Enemies/Scripts/EnemyFacade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Features.Enemies.Scripts
+{
+    public class EnemyFacade : MonoBehaviour
+    {
+        [Inject] private ICharacterProvider _characterProvider;
+
+        public void Update()
+        {
+            Vector3 direction = _characterProvider.CharacterFacade.transform.position - transform.position;
+            
+            direction.y = 0f;
+
+            float distance = direction.magnitude;
+            
+            if (distance <= 10)
+            {
+                return;
+            }
+            
+            Vector3 moveDirection = direction.normalized;
+            Vector3 newPosition = transform.position + moveDirection * 10 * Time.deltaTime;
+            
+            transform.position = newPosition;
+        }
+    }
+}
