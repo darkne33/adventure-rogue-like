d4a7b593883450509d4f2a7b8d0f9e7fcd9271e9
darkne33 <yuravislous@gmail.com>
2026-02-11 21:46:47 +0300

    health view update, add character model

--- Assets/Features/HealthView.cs
+++ Assets/Features/HealthView.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using Core;
+using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 public class HealthView : MonoBehaviour, IHealthView
 {
+    [SerializeField] private RectTransform _root;
     [SerializeField] private Slider _healthSlider;
+    
+    [Inject] private ICameraService _cameraService;
+    
+    private void Update()
+    {
+        _root.LookAt(new Vector3(_cameraService.MainCamera.transform.position.x, _root.position.y,
+            _cameraService.MainCamera.transform.position.z));
+    }
 
     public void UpdateHealth(int currentHealth, int maximumHealth)
     {
