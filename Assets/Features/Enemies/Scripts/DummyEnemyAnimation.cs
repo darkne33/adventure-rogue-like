536272a693cecc0581eee89e7743bf00a0ab5185
darkne33 <yuravislous@gmail.com>
2026-03-01 22:53:08 +0300

    camera distance, dummy run animation

--- Assets/Features/Enemies/Scripts/DummyEnemyAnimation.cs
+++ Assets/Features/Enemies/Scripts/DummyEnemyAnimation.cs
@@ -4,26 +4,22 @@ public class DummyEnemyAnimation : IEnemyAnimationSystem
 {
     private static readonly int IsRunning = Animator.StringToHash("IsRunning");
     private static readonly int Attack = Animator.StringToHash("Attack");
-    
+
     private readonly Animator _animator;
-    
+
     public DummyEnemyAnimation(Animator animator)
     {
         _animator = animator;
     }
-    
+
     public void IdleAnimation()
     {
-        
     }
 
-    public void RunAnimation()
-    {
-        
-    }
+    public void RunAnimation() =>
+        _animator.SetBool(IsRunning, true);
 
     public void AttackAnimation()
     {
-        
     }
 }
